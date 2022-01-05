3f96f8b6d5727cbbecab11a1f38472ff6388fb61
Jan Sporszil <jansporszil@gmail.com>
2022-01-05 01:08:27 +0100

    added script for submit teacherAvailability

---
+++ public/assets/js/teacherAvailability.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector('.addButton').addEventListener('click', function () {
+        const select1 = document.querySelector('.selectDay').value;
+        const select2 = document.querySelector('.selectTime').value;
+
+        if(select1 === "" || select2 === ""){
+            alert("Please choose both options");
+        }
+        else {
+            let dataToSent = new FormData();
+            dataToSent.append('day', select1);
+            dataToSent.append('time', select2);
+
+            fetch('/addAvailability', {
+                method: 'POST', body: dataToSent
+            })
+                .then(res => res.json())
+                .then(res => console.log(res))
+        }
+    })
+})
