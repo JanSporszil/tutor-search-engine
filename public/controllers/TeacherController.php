aee0ee73e64eac9d50d3de1275c91000f7ed6813
Jan Sporszil <jansporszil@gmail.com>
2022-01-05 01:06:00 +0100

    added TeacherController

---
+++ public/controllers/TeacherController.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once 'AppController.php';
+require_once __DIR__.'/../models/Users.php';
+require_once __DIR__.'/../repository/TeacherRepository.php';
+
+
+class TeacherController extends AppController
+{
+
+    private TeacherRepository $teacherRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->teacherRepository = new TeacherRepository();
+    }
+
+
+    public function addAvailability() {
+        $day = $_POST['day'];
+        $hour = $_POST['time'];
+
+        $user = $_SESSION['user'];
+
+        $availability = $this->teacherRepository->getAvail($user->getId());
+
+        $unserialized = unserialize($availability);
+
+        $unserialized[$day] = $hour;
+
+        $sendArray = serialize($unserialized);
+
+        $this->teacherRepository->addAvail($user->getId(), $sendArray);
+    }
+}
